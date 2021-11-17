32ea9b018f43031891d615ce9cf446d0162d1d71
Halim Wijaya <hwijaya@gmail.com>
2021-11-17 18:35:22 +0800

    corrected powershell

--- tasks/createheapdump.ps1
+++ tasks/createheapdump.ps1
@@ -5,11 +5,16 @@ Param(
   $Name 
 )
 
+$filepath=""
+#$Name="c:\temp"
+#write-output "first line"
 if ($Name -eq $null -or $Name -eq "") {
-  $filepath="c:\temp\heapdump$(get-date -f yyyyMMdd_HHmmss)"
+  $filepath="c:\temp\heapdump$(get-date -f yyyyMMdd_HHmmss).hprof"
 } else {
+ $filepath="$Name\newheapdump$(get-date -f yyyyMMdd_HHmmss).hprof"
+}
 
 $procid=get-process "Tomcat8" |select -expand id
 cd "C:\Program Files\Java\jdk1.8.0_202\bin"
-.\jmap.exe -dump:format=b,file=$filepath $procid
-}
+.\jmap.exe -dump:format=b,file=$filepath $procid 
+
