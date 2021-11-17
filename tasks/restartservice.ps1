46b7b0478df5d25acb1d2923ed249522163cfe50
Halim Wijaya <hwijaya@gmail.com>
2021-11-17 22:14:51 +0800

    update sleep

--- tasks/restartservice.ps1
+++ tasks/restartservice.ps1
@@ -8,6 +8,6 @@ Write-Output("Tomcat service was last restarted on $lastrestart GMT")
 Write-Output("Restarting Tomcat Service") 
 restart-service -name Tomcat8
 get-service -name Tomcat8
-Start-Sleep -s 5
+Start-Sleep -s 3
 $lastrestartnew= (Get-EventLog -LogName "System" -Source "Service Control Manager" -EntryType "Information" -Message "*Tomcat*running*" -Newest 1).TimeGenerated;
 Write-Output("Tomcat service was last restarted on $lastrestartnew GMT")
